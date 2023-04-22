0c348fe7f79c71d69a64a9aa6df94add3aeb14dc
John Pertoft <john.pertoft@gmail.com>
2023-04-22 19:00:50 +0200

    Set substituters.

--- hosts/nixos/home-desktop/configuration.nix
+++ hosts/nixos/home-desktop/configuration.nix
@@ -124,6 +124,16 @@
   #   "https://nixpkgs-unfree.cachix.org"
   # ];
 
+  nix.settings.substituters = [
+    "https://cuda-maintainers.cachix.org"
+    "https://nixpkgs-unfree.cachix.org"
+  ];
+
+  nix.settings.trusted-public-keys = [
+    "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+    "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
+  ];
+
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
   
