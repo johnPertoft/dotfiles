b69b8c4157a6515af14b8635c99ac2cb5bb3bd3b
John Pertoft <john.pertoft@gmail.com>
2023-04-22 20:16:19 +0200

    Remove double import of hardware config.

--- hosts/nixos/home-desktop/configuration.nix
+++ hosts/nixos/home-desktop/configuration.nix
@@ -5,11 +5,6 @@
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
-
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
