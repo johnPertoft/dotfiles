34af78b84b59c17d9264f4ee847761894f5f4078
John Pertoft <john.pertoft@gmail.com>
2023-04-25 16:41:28 +0200

    Add gnome systray icons.

--- hosts/nixos/work-desktop/configuration.nix
+++ hosts/nixos/work-desktop/configuration.nix
@@ -145,6 +145,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    gnomeExtensions.appindicator
     vim
     # cudatoolkit
     # cudaPackages.cudnn
