1f5803c3e3bf6cc8ac9ad740734bd0fca914b809
John Pertoft <john.pertoft@gmail.com>
2023-04-25 22:44:37 +0200

    Don't use wayland.

--- hosts/nixos/common.nix
+++ hosts/nixos/common.nix
@@ -29,10 +29,12 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  # TODO: Set gdm to default to x.
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.gdm = {
+    enable = true;
+    wayland = false;
+  };
 
   # Configure keymap in X11
   services.xserver = {
