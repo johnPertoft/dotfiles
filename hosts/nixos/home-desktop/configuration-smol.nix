740b6aef49f63776f8ae7eedf94d361b56e193ef
John Pertoft <john.pertoft@gmail.com>
2023-04-25 18:06:30 +0200

    Add small configs.

---
+++ hosts/nixos/home-desktop/configuration-smol.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, ... }:
+{
+  # Define hostname.
+  networking.hostName = "nixos";
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.john = {
+    isNormalUser = true;
+    description = "John Pertoft";
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
+    packages = with pkgs; [ ];
+  };
+}
