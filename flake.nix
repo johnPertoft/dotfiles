35e12a4fcbc1aff7f5edd94b20a67375684a50d7
John Pertoft <john.pertoft@gmail.com>
2023-09-17 01:51:02 +0200

    wip

--- flake.nix
+++ flake.nix
@@ -47,45 +47,4 @@
       #   };
       # };
     };
-
-  ########################
-  # outputs = { self, nixpkgs, nixpkgs-unstable, flake-utils, home-manager }:
-  #   {
-  #     nixosConfigurations = {
-  #       nixos = nixpkgs.lib.nixosSystem {
-  #         system = "x86_64-linux";
-  #         modules = [
-  #           ./hosts/nixos/common.nix
-  #           ./hosts/nixos/home-desktop/configuration.nix
-  #           ./hosts/nixos/home-desktop/hardware-configuration.nix
-  #         ]; 
-  #       };
-
-  #       nixos-work = nixpkgs.lib.nixosSystem {
-  #         system = "x86_64-linux";
-  #         modules = [
-  #           ./hosts/nixos/common.nix
-  #           ./hosts/nixos/work-desktop/configuration.nix
-  #           ./hosts/nixos/work-desktop/hardware-configuration.nix
-  #         ];
-  #       };
-  #     };
-
-  #     homeConfigurations = {
-  #       "john@x86_64-linux" = home-manager.lib.homeManagerConfiguration {
-  #         #pkgs = nixpkgs-unstable.legacyPackages.x86_64-linux;
-  #         pkgs = nixpkgs.legacyPackages.x86_64-linux;
-  #         modules = [ ./home-manager/home.nix ];
-  #       };
-  #     };
-  #   } // flake-utils.lib.eachDefaultSystem (system:
-  #     let
-  #       pkgs = nixpkgs.legacyPackages.${system};
-  #     in
-  #     {
-  #       formatter = pkgs.nixpkgs-fmt;
-  #       apps.default = import ./apps/hello.nix { inherit pkgs; inherit self; };
-  #       apps.switch-system = import ./apps/switch-system.nix { inherit pkgs; inherit self; };
-  #       apps.switch-home = import ./apps/switch-home.nix { inherit pkgs; inherit self; };
-  #     });
 }
