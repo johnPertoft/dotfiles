22d24979a4a29885841d131a98a2fa5088be9250
John Pertoft <john.pertoft@gmail.com>
2023-09-17 22:02:47 +0200

    wip

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,3 +1,6 @@
-{ nixpkgs, ... }: {
-    default = import ./home;
-}
+{ nixpkgs, ... }:
+let
+  names = builtins.attrNames (nixpkgs.lib.filterAttrs (n: v: v == "directory") (builtins.readDir ./.));
+  importModule = name: import ./${name};
+in
+nixpkgs.lib.genAttrs names importModule
