5ecadb9e9d10edb80e711fd9736e5e845932f6fc
John Pertoft <john.pertoft@gmail.com>
2023-10-15 00:42:38 +0200

    wip

--- homes/john/home.nix
+++ homes/john/home.nix
@@ -3,12 +3,10 @@
     home.homeDirectory = "/home/john";
     programs.git.userName = "John Pertoft";
     programs.git.userEmail = "john.pertoft@gmail.com";
-#   imports = [
-#     self.homeModules.vscode
-#     self.homeModules.vim
-#     self.homeModules.neovim
-#     self.homeModules.emacs
-#     self.homeModules.git
-#     self.homeModules.packages
-#   ];
+    imports = [
+        self.homeModules.vscode
+        self.homeModules.vim
+        self.homeModules.git
+        self.homeModules.packages
+    ];
 }
--- modules/home-manager/git/default.nix
+++ modules/home-manager/git/default.nix
@@ -2,17 +2,20 @@
   programs.git = {
     enable = true;
     package = pkgs.gitFull;
+    aliases = {
+      s = "status";
+      l = "log";
+      d = "diff";
+    };
     lfs = {
       enable = true;
       skipSmudge = true;
     };
     extraConfig = {
+      core.editor = "vim";
       init.defaultBranch = "main";
       pull.ff = "only";
       push.autoSetupRemote = true;
-      core.editor = "vim";
     };
-    userName = "John Pertoft";
-    userEmail = "john.pertoft@gmail.com";
   };
 }
