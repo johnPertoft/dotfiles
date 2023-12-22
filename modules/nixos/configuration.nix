7126a095d07e93a3e07e3085e5f6c641f13fde52
John Pertoft <john.pertoft@gmail.com>
2023-12-22 17:05:06 +0100

    Hmm

--- modules/nixos/configuration.nix
+++ modules/nixos/configuration.nix
@@ -134,7 +134,7 @@
 
   services.tailscale.enable = true;
   networking.nameservers = [ "100.100.100.100" "8.8.8.8" "1.1.1.1" ];
-  # networking.search = [ "example.ts.net" ];
+  networking.search = [ "example.ts.net" ];
 
   # Enable Docker container runtime.
   virtualisation.docker = {
