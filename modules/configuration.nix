52b93810714efdc5f298d07ea267a389b5acdaef
John Pertoft <john.pertoft@gmail.com>
2025-11-23 14:36:37 +0100

    wip

--- modules/configuration.nix
+++ modules/configuration.nix
@@ -7,15 +7,17 @@
       "flakes"
     ];
     substituters = [
+      "https://nix-community.cachix.org"
       "https://numtide.cachix.org"
       "https://devenv.cachix.org"
     ];
     trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
       "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
     ];
-    cores = 4;
-    max-jobs = 1;
+    cores = 0;
+    max-jobs = "auto";
   };
 
   # Link old commands (nix-shell, nix-build, etc.) to use the same nixpkgs as the flake.
