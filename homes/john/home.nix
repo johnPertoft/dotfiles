a2808bc510389c829bf22479d6b55720ca11893a
John Pertoft <john.pertoft@gmail.com>
2025-12-23 08:23:02 +0100

    wip

--- homes/john/home.nix
+++ homes/john/home.nix
@@ -6,6 +6,6 @@
 {
   home.username = "john";
   home.homeDirectory = if pkgs.stdenv.hostPlatform.isDarwin then "/Users/john" else "/home/john";
-  programs.git.settings.userName = "John Pertoft";
-  programs.git.settings.userEmail = "john.pertoft@gmail.com";
+  programs.git.settings.user.name = "John Pertoft";
+  programs.git.settings.user.email = "john.pertoft@gmail.com";
 }
