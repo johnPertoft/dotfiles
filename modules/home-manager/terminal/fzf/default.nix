b255e90c55c51a9d8e823c1bdf3ef8b93c380756
John Pertoft <john.pertoft@gmail.com>
2026-07-18 09:49:59 +0200

    fzf preview

--- modules/home-manager/terminal/fzf/default.nix
+++ modules/home-manager/terminal/fzf/default.nix
@@ -1,22 +1,64 @@
 { config
 , pkgs
+, lib
 , self
 , ...
 }:
+let
+  # junegunn's official "preview anything" helper (bat for text, tree for dirs,
+  # chafa for images). Wrapped so the tools it needs are on PATH regardless of
+  # what's installed globally.
+  fzf-preview =
+    let
+      src = pkgs.fetchurl {
+        # Pinned to a release tag (not master) so the hash can't drift under us.
+        url = "https://raw.githubusercontent.com/junegunn/fzf/v0.74.0/bin/fzf-preview.sh";
+        hash = "sha256-DkoKSJtUfap13Y7u/WtShgZ3QBsrd1T938txoYZdRVI=";
+        executable = true;
+      };
+    in
+    pkgs.writeShellScriptBin "fzf-preview.sh" ''
+      export PATH="${lib.makeBinPath [ pkgs.bat pkgs.file pkgs.chafa pkgs.coreutils ]}:$PATH"
+      export BAT_THEME="ansi"
+      exec ${src} "$@"
+    '';
+in
 {
   programs.fzf = {
     enable = true;
     tmux.enableShellIntegration = config.programs.tmux.enable;
     defaultOptions = [ "--height 100%" ];
+
+    # CTRL-T: file picker with rich content/image previews.
+    fileWidgetCommand = "fd --type f --color=always";
     fileWidgetOptions = [
-      "--preview 'stat {}'"
-      "--preview-window noborder"
+      "--ansi"
+      "--preview '${lib.getExe fzf-preview} {}'"
+      "--bind 'focus:transform-preview-label:echo {}'"
+      "--bind '?:toggle-preview'"
+    ];
+
+    # ALT-C: directory picker with a tree preview.
+    changeDirWidgetCommand = "fd --type d --color=always";
+    changeDirWidgetOptions = [
+      "--ansi"
+      "--preview 'tree -C {}'"
+      "--bind 'focus:transform-preview-label:echo {}'"
+      "--bind '?:toggle-preview'"
+    ];
+
+    # CTRL-R: history, preview hidden by default (? to peek).
+    historyWidgetOptions = [
+      "--preview 'echo {}'"
+      "--preview-window hidden"
+      "--bind '?:toggle-preview'"
     ];
   };
 
   home.packages = with pkgs; [
     fzf-git-sh
     bat
+    tree
     self.packages.${pkgs.stdenv.hostPlatform.system}.fzf-open
     self.packages.${pkgs.stdenv.hostPlatform.system}.fzf-jq
   ];
