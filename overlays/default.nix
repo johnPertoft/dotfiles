a5e078a1a3cf0c41e0cdcb0c14b0335c3c5765fa
John Pertoft <john.pertoft@gmail.com>
2026-09-01 14:33:28 +0200

    overlay cleanup

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,5 +2,5 @@
 {
   nixpkgs-unstable = import ./nixpkgs-unstable.nix { inherit nixpkgs-unstable; };
   modules-closure = import ./modules-closure.nix { };
-  pre-commit-no-tests-darwin = import ./pre-commit-no-tests-darwin.nix;
+  pre-commit-darwin = import ./patches/pre-commit-darwin.nix;
 }
